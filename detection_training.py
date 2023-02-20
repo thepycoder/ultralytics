19c2b3b50bbbbbde26e57722f07cf6d99d315dd8
Victor Sonck <victor.sonck@gmail.com>
2023-02-20 16:54:03 +0100

    Add simple detection script

---
+++ detection_training.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+results = model.train(data="coco128.yaml", epochs=10, imgsz=640, task='detect')  # train the model
